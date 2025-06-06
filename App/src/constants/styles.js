6c7925ab5e51b4477c158bf9523370599cedafae
FabricioGardin <70030723+FabricioGardin@users.noreply.github.com>
2025-06-05 22:45:44 -0700

    Create styles.js

---
+++ App/src/constants/styles.js
@@ -0,0 +1,74 @@
+import { Colors } from "./colors.js";
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    marginRight: 20,
+    marginLeft: 20,
+    height: "100%",
+    width: "100%",
+  },
+  text: {
+    fontSize: 20,
+    marginVertical: 50,
+    color: "Black",
+    fontWeight: "bold",
+  },
+
+  textInput: { padding: 15 },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 50,
+    width: "90%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+  },
+  backLogin: {
+    textAlign: "left",
+    fontSize: 16,
+    color: Colors.PRIMARY,
+    fontWeight: "bold",
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: Colors.PRIMARY,
+    color: "white",
+    padding: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    marginTop: 30,
+    width: "90%",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  newAccount: {
+    textAlign: "center",
+    marginTop: 40,
+    color: "blue",
+  },
+  backText: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  imageLamp: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    marginTop: 25,
+    marginEnd: 30,
+    alignSelf: "left",
+  },
+});
+
+export { Styles };
--- App/src/screens/SignUp.js
+++ App/src/screens/SignUp.js
@@ -9,6 +9,8 @@ import {
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { Image } from "react-native";
 import { useState } from "react";
+import { Colors } from "../constants/colors.js";
+import { Styles } from "../constants/styles.js";
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -21,50 +23,50 @@ const SignUp = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={Styles.container}>
       <Image
         source={require("../../assets/lamp.jpg")}
-        style={styles.imageLamp}
+        style={Styles.imageLamp}
       />
       {/* Form Sign Up*/}
-      <View style={styles.backText}>
-        <Icon name="arrow-left" size={24} color="blue" />
-        <Text style={styles.backLogin}>BACK TO LOGIN</Text>
+      <View style={Styles.backText}>
+        <Icon name="arrow-left" size={24} color={Colors.PRIMARY} />
+        <Text style={Styles.backLogin}>BACK TO LOGIN</Text>
       </View>
-      <Text style={styles.text}>Sign UP</Text>
-      <View style={styles.inputContainer}>
+      <Text style={Styles.text}>Sign UP</Text>
+      <View style={Styles.inputContainer}>
         <Icon name="account" size={24} color="blue" />
         <TextInput
-          style={styles.textInput}
+          style={Styles.textInput}
           placeholder="Name"
           value={name}
           onChangeText={setName}
         />
       </View>
-      <View style={styles.inputContainer}>
+      <View style={Styles.inputContainer}>
         <Icon name="email-outline" size={24} color="blue" />
         <TextInput
-          style={styles.textInput}
+          style={Styles.textInput}
           placeholder="Email"
           keyboardType="email-address"
           value={email}
           onChangeText={setEmail}
         />
       </View>
-      <View style={styles.inputContainer}>
+      <View style={Styles.inputContainer}>
         <Icon name="lock-outline" size={20} color="blue" />
         <TextInput
-          style={styles.textInput}
+          style={Styles.textInput}
           placeholder="Password"
           secureTextEntry
           value={password}
           onChangeText={setPassword}
         />
       </View>
-      <View style={styles.inputContainer}>
+      <View style={Styles.inputContainer}>
         <Icon name="lock-outline" size={20} color="blue" />
         <TextInput
-          style={styles.textInput}
+          style={Styles.textInput}
           placeholder="Confirm Password"
           secureTextEntry
           value={confirmPassword}
@@ -73,85 +75,82 @@ const SignUp = () => {
       </View>
 
       <View>
-        <Pressable
-          style={styles.btnSignUp}
-          onPress={handleSignUp}
-        >
-          <Text style={styles.btnSignUpText}>Sign Up</Text>
+        <Pressable style={Styles.button} onPress={handleSignUp}>
+          <Text style={Styles.buttonText}>Sign Up</Text>
         </Pressable>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    marginRight: 20,
-    marginLeft: 20,
-    height: "100%",
-    width: "100%",
-  },
-  text: {
-    fontSize: 20,
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     marginRight: 20,
+//     marginLeft: 20,
+//     height: "100%",
+//     width: "100%",
+//   },
+//   text: {
+//     fontSize: 20,
 
-    marginVertical: 50,
-    color: "blue",
-    fontWeight: "bold",
-  },
+//     marginVertical: 50,
+//     color: Colors.PRIMARY,
+//     fontWeight: "bold",
+//   },
 
-  textInput: { padding: 15 },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    height: 50,
-    width: "90%",
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-  },
-  backLogin: {
-    textAlign: "left",
-    fontSize: 16,
-    color: "blue",
-    fontWeight: "bold",
-    marginLeft: 10,
-  },
-  btnSignUp: {
-    backgroundColor: "blue",
-    color: "white",
-    padding: 10,
-    borderRadius: 15,
-    alignItems: "center",
-    marginTop: 30,
-    width: "90%",
-  },
-  btnSignUpText: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  newAccount: {
-    textAlign: "center",
-    marginTop: 40,
-    color: "blue",
-  },
-  backText: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 40,
-  },
-  imageLamp: {
-    width: 70,
-    height: 70,
-    borderRadius: 10,
-    marginTop: 25,
-    marginEnd: 30,
-    alignSelf: "left",
-  },
-});
+//   textInput: { padding: 15 },
+//   inputContainer: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     height: 50,
+//     width: "90%",
+//     borderColor: "gray",
+//     borderWidth: 1,
+//     marginBottom: 20,
+//     paddingHorizontal: 10,
+//     borderRadius: 10,
+//   },
+//   backLogin: {
+//     textAlign: "left",
+//     fontSize: 16,
+//     color: Colors.PRIMARY,
+//     fontWeight: "bold",
+//     marginLeft: 10,
+//   },
+//   btnSignUp: {
+//     backgroundColor: Colors.PRIMARY,
+//     color: "white",
+//     padding: 10,
+//     borderRadius: 15,
+//     alignItems: "center",
+//     marginTop: 30,
+//     width: "90%",
+//   },
+//   btnSignUpText: {
+//     color: "white",
+//     textAlign: "center",
+//     fontSize: 18,
+//     fontWeight: "bold",
+//   },
+//   newAccount: {
+//     textAlign: "center",
+//     marginTop: 40,
+//     color: "blue",
+//   },
+//   backText: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     marginTop: 40,
+//   },
+//   imageLamp: {
+//     width: 70,
+//     height: 70,
+//     borderRadius: 10,
+//     marginTop: 25,
+//     marginEnd: 30,
+//     alignSelf: "left",
+//   },
+// });
 export default SignUp;
