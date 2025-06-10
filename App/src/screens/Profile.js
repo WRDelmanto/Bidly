a1e252c3dc22b1a436bb7917bd9bff170e3a5f99
FabricioGardin <70030723+FabricioGardin@users.noreply.github.com>
2025-06-09 22:33:43 -0700

    Create profile  screen

--- App/src/screens/Profile.js
+++ App/src/screens/Profile.js
@@ -1,17 +1,65 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import NavBar from "../components/Navbar";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import { AppStyles } from "../constants/styles";
 
 const Profile = () => {
   return (
-    <Text
-      style={{
-        textAlign: "center",
-        textAlignVertical: "center",
-        flex: 1,
-      }}
-    >
-      This is the Profile Screen
-    </Text>
+    <View style={AppStyles.container}>
+      <Image
+        style={styles.imageContainer}
+        source={require("../../assets/motorcycle.jpg")}
+        resizeMode="contain"
+      />
+      <View style={styles.profileInfo}>
+        <Icon
+          name="account-circle"
+          size={30}
+          onPress={() => console.log("Profile clicked")}
+        />
+        <Text>Bid</Text>
+        <Text>Title</Text>
+        <Text>Description</Text>
+        <View style={styles.dateContainer}>
+          <Icon name="calendar-today" size={30} />
+
+          <Text>Due Date</Text>
+        </View>
+      </View>
+      <NavBar />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: "50%",
+    width: "80%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileInfo: {
+    position: "absolute",
+    bottom: 60,
+    left: 0,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    paddingBottom: 12,
+    alignItems: "start",
+    paddingHorizontal: 20,
+    gap: 8,
+  },
+  dateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginTop: 8,
+    margemBottom: 20,
+  },
+});
 
 export default Profile;
