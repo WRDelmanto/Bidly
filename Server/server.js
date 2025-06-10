bcf9881c92cda785b618abe43590e664d63fd419
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-06-10 09:39:31 -0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 app.use("/api", router);
 
-const main = async () => {
+async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
@@ -21,10 +21,3 @@ const main = async () => {
     console.error("Error connecting to MongoDB:", error);
   }
 };
-
-main();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
