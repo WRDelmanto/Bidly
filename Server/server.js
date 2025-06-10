2ffc7718f8f976ce240ef0cd2e4ef2aea948e2f4
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-06-10 10:01:18 -0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -21,3 +21,8 @@ async () => {
     console.error("Error connecting to MongoDB:", error);
   }
 };
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
