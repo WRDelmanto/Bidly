8a1bf9320653cdbb5401c98a4dd4f06f2dff63e2
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-06-14 16:52:48 -0700

    Fix Profile Style

--- App/src/screens/Profile.js
+++ App/src/screens/Profile.js
@@ -1,7 +1,6 @@
 import { Text, View, StyleSheet, Pressable } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppStyles } from "../constants/styles";
-import { AppColors } from "../constants/colors";
 import { ENDPOINTS } from "../constants/api.js";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import NavBar from "../components/NavBar";
@@ -60,101 +59,108 @@ const Profile = ({ navigation }) => {
       console.error('Error signing off:', error);
     }
   };
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={AppStyles.mainContainer}>
-        <View style={styles.header}>
-          <View>
-            <Icon style={styles.avatarContainer} name="account-circle" size={80} color={AppColors.PRIMARY} />
-          </View>
-          <View style={styles.statisticContainer}>
-            <View style={styles.statisticInfo}>
-              <Text style={styles.statisticValue}>{user?.stats.createdAuctions}</Text>
-              <Text style={styles.statisticValue}>aucts</Text>
-            </View>
-            <View style={styles.statisticInfo}>
-              <Text style={styles.statisticValue}>{user?.stats.bids}</Text>
-              <Text style={styles.statisticValue}>bids</Text>
-            </View>
-            <View style={styles.statisticInfo}>
-              <Text style={styles.statisticValue}>{user?.stats.wonAuctions}</Text>
-              <Text style={styles.statisticValue}>won</Text>
-            </View>
-          </View>
-          <Pressable style={styles.configIcon} onPress={() => setShowDropdown(!showDropdown)}>
-            <Icon name="cog" size={40} color={AppColors.PRIMARY} />
-          </Pressable>
-          {showDropdown && (
-            <View style={styles.dropdown}>
-              <Pressable style={styles.dropdownItem} onPress={handleEditProfile}>
-                <Icon name="account-edit" size={20} color={AppColors.PRIMARY} style={styles.dropdownIcon} />
-                <Text style={styles.dropdownText}>Edit Profile</Text>
-              </Pressable>
-              <Pressable style={styles.dropdownItem} onPress={handleSignOff}>
-                <Icon name="logout" size={20} color={AppColors.PRIMARY} style={styles.dropdownIcon} />
-                <Text style={styles.dropdownText}>Logoff</Text>
-              </Pressable>
-            </View>
-          )}
 
+  return (
+    <View style={AppStyles.mainContainer}>
+      <View style={styles.profileInfoContainer}>
+        <View style={styles.pictureStatsContainer}>
+          <Icon
+            name="account-circle"
+            size={80}
+          />
+          <View style={styles.statsContainer}>
+            <View style={styles.statsInfo}>
+              <Text style={styles.statsText}>{user?.stats.createdAuctions}</Text>
+              <Text style={styles.statsSubText}>aucts</Text>
+            </View>
+            <View style={styles.statsInfo}>
+              <Text style={styles.statsText}>{user?.stats.bids}</Text>
+              <Text style={styles.statsSubText}>bids</Text>
+            </View>
+            <View style={styles.statsInfo}>
+              <Text style={styles.statsText}>{user?.stats.wonAuctions}</Text>
+              <Text style={styles.statsSubText}>won</Text>
+            </View>
+          </View>
         </View>
-        <View>
-          <Text style={styles.title}>{user?.name}</Text>
-        </View>
+        <Pressable
+          style={styles.configIcon}
+          onPress={() => setShowDropdown(!showDropdown)}>
+          <Icon
+            name="cog"
+            size={28}
+          />
+        </Pressable>
+        {showDropdown && (
+          <View style={styles.dropdown}>
+            <Pressable style={styles.dropdownItem} onPress={handleEditProfile}>
+              <Icon
+                name="account-edit"
+                size={20}
+                style={styles.dropdownIcon}
+              />
+              <Text style={styles.dropdownText}>Edit Profile</Text>
+            </Pressable>
+            <Pressable style={styles.dropdownItem} onPress={handleSignOff}>
+              <Icon name="logout"
+                size={20}
+                style={styles.dropdownIcon}
+              />
+              <Text style={styles.dropdownText}>Logoff</Text>
+            </Pressable>
+          </View>
+        )}
       </View>
-      <View style={styles.navBar}>
-        <NavBar navigation={navigation} />
+      <View>
+        <Text style={styles.userName}>{user?.name}</Text>
       </View>
-    </View>
+      <NavBar navigation={navigation} />
+    </View >
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    alignItems: "center",
+  profileInfoContainer: {
+    flexDirection: 'column',
   },
-  avatarContainer: {
-    position: "relative",
-  },
-  configIcon: {
-    position: "absolute",
-    top: 5,
-    right: 5,
-    padding: 5,
-  },
-  statisticInfo: {
-    alignItems: "center",
-  },
-  statisticValue: {
-    fontSize: 20,
-    fontWeight: "bold",
-
-  },
-  statisticContainer: {
+  pictureStatsContainer: {
+    width: "70%",
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    gap: 15,
-    marginLeft: 30,
+    alignContent: "left"
   },
-  title: {
+  statsContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  statsInfo: {
+    alignItems: "center",
+  },
+  statsText: {
+    fontSize: 16,
+  },
+  statsSubText: {
+    fontSize: 14,
+  },
+  userName: {
     fontSize: 20,
     color: "Black",
     fontWeight: "bold",
-    marginLeft: 20,
+    marginLeft: 8,
   },
-  navBar: {
+  configIcon: {
     position: "absolute",
-    bottom: 0,
-    left: 0,
+    top: 0,
     right: 0,
   },
   dropdown: {
     position: "absolute",
-    top: 60,
-    right: 10,
-    backgroundColor: "#fff",
+    top: 25,
+    right: 25,
+    backgroundColor: "#FFFFFF",
     borderRadius: 8,
     elevation: 5,
     paddingVertical: 10,
@@ -171,9 +177,7 @@ const styles = StyleSheet.create({
   },
   dropdownText: {
     fontSize: 16,
-    color: AppColors.PRIMARY,
   },
-
 });
 
 export default Profile;
