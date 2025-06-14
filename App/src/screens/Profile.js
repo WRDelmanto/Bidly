47f03c7f8b2c162adde4bc4a36583e428e328b82
FabricioGardin <70030723+FabricioGardin@users.noreply.github.com>
2025-06-13 19:20:20 -0700

    Created profile page

--- App/src/screens/Profile.js
+++ App/src/screens/Profile.js
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppStyles } from "../constants/styles";
+import { AppColors } from "../constants/colors";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import NavBar from "../components/NavBar";
 
 const Profile = ({ navigation }) => {
   const handleSignOff = async () => {
@@ -11,17 +15,86 @@ const Profile = ({ navigation }) => {
     }
   };
   return (
-    <Text
-      style={{
-        textAlign: "center",
-        textAlignVertical: "center",
-        flex: 1,
-      }}
-      onPress={handleSignOff}
-    >
-      This is the Profile Screen
-    </Text>
+    <View style={{ flex: 1 }}>
+      <View style={AppStyles.container}>
+        <View style={styles.header}>
+          <View>
+            <Icon style={styles.avatarContainer} name="account-circle" size={80} color={AppColors.PRIMARY} />
+          </View>
+          <View style={styles.statisticContainer}>
+            <View style={styles.statisticInfo}>
+              <Text style={styles.statisticValue}>10</Text>
+              <Text style={styles.statisticValue}>AUCTS</Text>
+            </View>
+            <View style={styles.statisticInfo}>
+              <Text style={styles.statisticValue}>40</Text>
+              <Text style={styles.statisticValue}>BIDS</Text>
+            </View>
+            <View style={styles.statisticInfo}>
+              <Text style={styles.statisticValue}>5</Text>
+              <Text style={styles.statisticValue}>WON</Text>
+            </View>
+          </View>
+          <Pressable style={styles.configIcon}>
+            <Icon name="cog" size={40} color={AppColors.PRIMARY} />
+          </Pressable>
+        </View>
+        <View>
+          <Text style={styles.title}>Name</Text>
+        </View>
+      </View>
+      <View style={styles.navBar}>
+        <NavBar />
+      </View>
+    </View>
   );
+
+ 
 };
+
+ const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: "center",
+  },
+  avatarContainer: {
+    position: "relative",
+  },
+  configIcon: {
+    position: "absolute",
+    top: 5,
+    right: 5,    
+    padding: 5,
+  },
+  statisticInfo:{
+    alignItems: "center",
+  },
+  statisticValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+    
+  },
+  statisticContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap:15,
+    marginLeft: 30,
+  },
+  title: {
+    fontSize: 20,
+    color: "Black",
+    fontWeight: "bold",
+    marginLeft: 20,
+  },
+  navBar: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  
+  },
+
+  });
 
 export default Profile;
