739000301d2de98c6bb84945d68f6d2e88c59ac5
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-06-15 16:00:51 -0700

    Update api.js

--- App/src/constants/api.js
+++ App/src/constants/api.js
@@ -1,10 +1,12 @@
 // API Configuration
-export const API_URL = 'https://bidly.onrender.com/api';
+export const API_URL = 'https://bidly.onrender.com/api'; // Render
+// export const API_URL = 'http://10.0.2.2:3000/api'; // Android emulator localhost
+// export const API_URL = 'http://localhost:3000/api'; // iOS simulator localhost
 
 // API Endpoints
 export const ENDPOINTS = {
     PING: `${API_URL}/ping`,
-    SIGNUP: `${API_URL}/signup`,
+    SIGNUP: `${API_URL}/signUp`,
     SIGNIN: `${API_URL}/signIn`,
     AUCTION: `${API_URL}/auction`,
     AUCTIONS: `${API_URL}/auctions`,
