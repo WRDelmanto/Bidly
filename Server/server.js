29052e67e8ccaf098e21744dea62546af0a12641
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-06-15 15:59:46 -0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -27,7 +27,7 @@ const connectDB = async () => {
 // Call the connect function
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
