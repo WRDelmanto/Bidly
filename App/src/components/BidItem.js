021545a495a0a572faf09f1135cd624efafefe0b
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-07-07 10:33:35 -0700

    Update BidItem.js

--- App/src/components/BidItem.js
+++ App/src/components/BidItem.js
@@ -1,16 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { AppColors } from "../constants/colors";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const BidItem = ({ bid }) => {
     return (
         <View style={styles.bidItem}>
-            <Text style={styles.bidderName}>
-                {bid.bidder?.name || 'Anonymous'}
-            </Text>
+            <View style={styles.bidderInfo}>
+                {bid?.bidder?.picture ? (
+                    <Image
+                        source={{ uri: bid?.bidder?.picture }}
+                        style={{ width: 40, height: 40, borderRadius: 100 }}
+                    />
+                ) : (
+                    <Icon name="account-circle" size={40} />
+                )}
+                <Text style={styles.bidderName}>
+                    {bid.bidder?.name}
+                </Text>
+            </View>
             <Text style={styles.bidAmount}>${bid.amount.toFixed(2)}</Text>
-            <Text style={styles.bidDate}>
-                {new Date(bid.createdAt).toLocaleDateString()}
-            </Text>
         </View>
     );
 };
@@ -21,23 +29,23 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingVertical: 12,
-        paddingHorizontal: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: '#e0e0e0',
+        borderTopWidth: 1,
+        borderTopColor: '#e0e0e0',
         backgroundColor: '#ffffff'
+    },
+    bidderInfo: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 6
     },
     bidderName: {
         fontSize: 16,
-        fontWeight: 'bold',
     },
     bidAmount: {
         fontSize: 16,
         fontWeight: 'bold',
         color: AppColors.PRIMARY,
-    },
-    bidDate: {
-        fontSize: 12,
-        color: '#666'
     },
 });
 
