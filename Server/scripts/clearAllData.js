db01e4692ef065c2241f9d06e5ed2820508d7c75
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-07-09 19:35:59 -0700

    Create clearAllData.js

---
+++ Server/scripts/clearAllData.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import User from "../models/user.js";
+import Auction from "../models/auction.js";
+import Bid from "../models/bid.js";
+
+dotenv.config();
+
+const clearAllData = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+
+        // await User.deleteMany({});
+        // console.log("User collections cleared successfully.");
+
+        // await Auction.deleteMany({});
+        // console.log("Auction collections cleared successfully.");
+
+        // await Bid.deleteMany({});
+        // console.log("Bid collections cleared successfully.");
+    } catch (error) {
+        console.error("Error clearing data:", error);
+    } finally {
+        await mongoose.disconnect();
+        console.log("Disconnected from MongoDB");
+    }
+};
+
+clearAllData();
